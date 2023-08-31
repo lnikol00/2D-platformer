b25fa616cef05e8e908803040ca4fd29252f0bc2
Koce1 <122328343+lnikol00@users.noreply.github.com>
2023-08-31 14:48:41 +0200

    Adding Finish and Start

---
+++ Assets/Scripts/Finnish.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finnish : MonoBehaviour
+{
+    AudioSource finishSoundEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        finishSoundEffect = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            finishSoundEffect.Play();
+            // CompleteLevel();
+        }
+    }
+
+    // void CompleteLevel()
+    // {
+
+    // }
+}
