f52d12df9c87432475fa5a04f2847301e965bff8
Koce1 <122328343+lnikol00@users.noreply.github.com>
2023-09-04 17:05:28 +0200

    Adding New Camera Movement

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class GameManager : MonoBehaviour
 {
+    public CinemachineVirtualCamera VCam;
     public GameObject[] playerPrefabs;
     int characterIndex;
     public static Vector2 checkPoint = new Vector2(-6, 2);
@@ -11,7 +13,8 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         characterIndex = PlayerPrefs.GetInt("SelectedCharacter", 0);
-        Instantiate(playerPrefabs[characterIndex], checkPoint, Quaternion.identity);
+        GameObject player = Instantiate(playerPrefabs[characterIndex], checkPoint, Quaternion.identity);
+        VCam.m_Follow = player.transform;
     }
 
 
