cba3df7abbb1301773bbfb54768c6d806a3cc7c1
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2024-09-01 00:33:03 +0200

    Fixing bugs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -10,16 +8,12 @@ public class PlayerMovement : MonoBehaviour
     Animator anim;
 
     public LayerMask jumpableGround;
- 
+
     private float dirX = 0f;
-    public float moveSpeed = 7f;
+    public float moveSpeed = 5f;
     public float jumpForce = 7f;
 
-    private bool doubleJump;
-    private int airJumpCount;
-    private int airJumpCountMax;
-
-    private enum MovementState { idle, running, jumping, falling, doubleJump }
+    private enum MovementState { idle, running, jumping, falling }
 
     public AudioSource jumpSoundEffect;
 
@@ -31,38 +25,18 @@ public class PlayerMovement : MonoBehaviour
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
 
-        airJumpCountMax = 1;
     }
     // Update is called once per frame
     private void Update()
     {
-        if(IsGrounded() && !Input.GetButtonDown("Jump"))
-        {
-            airJumpCount = 0;
-        }
 
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButton("Jump"))
+        if (Input.GetButton("Jump") && IsGrounded())
         {
-            if(IsGrounded())
-            {
-                jumpSoundEffect.Play();
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            }
-            else
-            {
-                if(Input.GetButtonDown("Jump"))
-                {
-                    if(airJumpCount < airJumpCountMax)
-                    {
-                        jumpSoundEffect.Play();
-                        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-                        airJumpCount++;
-                    }
-                }
-            }
+            jumpSoundEffect.Play();
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
         UpdateAnimationState();
@@ -72,7 +46,7 @@ public class PlayerMovement : MonoBehaviour
     {
         MovementState state;
 
-        if(dirX > 0f)
+        if (dirX > 0f)
         {
             state = MovementState.running;
             sprite.flipX = false;
@@ -87,23 +61,13 @@ public class PlayerMovement : MonoBehaviour
             state = MovementState.idle;
         }
 
-        if(rb.velocity.y > .1f)
+        if (rb.velocity.y > .1f)
         {
             state = MovementState.jumping;
-
-            if(airJumpCount == 1)
-            {
-                 state = MovementState.doubleJump;
-            }
         }
         else if (rb.velocity.y < -.1f)
         {
             state = MovementState.falling;
-
-            if(airJumpCount == 1)
-            {
-                 state = MovementState.doubleJump;
-            }
         }
 
         anim.SetInteger("state", (int)state);
@@ -111,6 +75,6 @@ public class PlayerMovement : MonoBehaviour
 
     private bool IsGrounded()
     {
-       return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
